3936634c49c80e91b70b7595ed0272f77b94fa19
William Froes <froes.ws@gmail.com>
2022-07-23 21:23:13 -0300

    Adjusting commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [ new Recipe("Test Recipe", "Just a Test Recipe", "https://aheadofourthyme.com/wp-content/uploads/2022/04/air-fryer-salmon-recipe-3.jpg")];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
